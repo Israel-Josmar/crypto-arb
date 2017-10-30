2da9447c8cdda4c8848948981c8f3d8332f2718d
Israel Lima <israelhenrique@gmail.com>
2017-10-30 17:19:26 -0200

    add method to get ask and bid prices

--- src/model/model.js
+++ src/model/model.js
@@ -11,6 +11,46 @@ export const getPrice = (currency_pair, exchange, callback) => {
   }
 }
 
+export const getAskPrice = (currency_pair, exchange, callback) => {
+  switch (exchange) {
+    case 'braziliex':
+        getAskPriceBrex(currency_pair, callback)
+      break;
+    default:
+
+  }
+}
+
+export const getBidPrice = (currency_pair, exchange, callback) => {
+  switch (exchange) {
+    case 'braziliex':
+        getBidPriceBrex(currency_pair, callback)
+      break;
+    default:
+
+  }
+}
+
+const getAskPriceBrex = (currency_pair ,callback) => {
+  const uri = 'https://braziliex.com/api/v1/public/orderbook/'+currency_pair
+  const options = { url: uri, json: true }
+  request(options, (error, response, json) => {
+      if (!error && response.statusCode === 200) {
+        callback(json.asks[0].price)
+      }
+    })
+}
+
+const getBidPriceBrex = (currency_pair ,callback) => {
+  const uri = 'https://braziliex.com/api/v1/public/orderbook/'+currency_pair
+  const options = { url: uri, json: true }
+  request(options, (error, response, json) => {
+      if (!error && response.statusCode === 200) {
+        callback(json.bids[0].price)
+      }
+    })
+}
+
 const getPriceBrex = (currency_pair ,callback) => {
   const uri = 'https://braziliex.com/api/v1/public/ticker/'+currency_pair
   const options = { url: uri, json: true }
--- src/model/model.test.js
+++ src/model/model.test.js
@@ -1,10 +1,26 @@
 import {
   getPrice,
+  getAskPrice,
+  getBidPrice,
 } from './model'
 
 test('shows get latest traded price', () => {
   getPrice('ltc_brl','braziliex', (lastPrice) => {
     expect(lastPrice).toEqual('184.00000000')
+  })
+
+})
+
+test('get best ask price', () => {
+  getAskPrice('ltc_brl','braziliex', (askPrice) => {
+    expect(askPrice).toEqual(186.9)
+  })
+
+})
+
+test('get best bid price', () => {
+  getBidPrice('ltc_brl','braziliex', (askPrice) => {
+    expect(askPrice).toEqual(184)
   })
 
 })
