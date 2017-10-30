0ea9426c6327300bfe5a8bcb0b86fdb4a8ee0663
Israel Lima <israelhenrique@gmail.com>
2017-10-30 17:10:30 -0200

    fix uri and getPrice

--- src/model/model.js
+++ src/model/model.js
@@ -3,8 +3,8 @@ const request = require('request')
 
 export const getPrice = (currency_pair, exchange, callback) => {
   switch (exchange) {
-    case exchange: 'braziliex'
-        return getPriceBrex(currency_pair, callback)
+    case 'braziliex':
+        getPriceBrex(currency_pair, callback)
       break;
     default:
 
@@ -12,8 +12,8 @@ export const getPrice = (currency_pair, exchange, callback) => {
 }
 
 const getPriceBrex = (currency_pair ,callback) => {
-  const link = '/public/ticker/'+currency_pair
-  const options = { url: link, json: true }
+  const uri = 'https://braziliex.com/api/v1/public/ticker/'+currency_pair
+  const options = { url: uri, json: true }
   request(options, (error, response, json) => {
       if (!error && response.statusCode === 200) {
         callback(json.last)
--- src/model/model.test.js
+++ src/model/model.test.js
@@ -4,7 +4,7 @@ import {
 
 test('shows get latest traded price', () => {
   getPrice('ltc_brl','braziliex', (lastPrice) => {
-    expect(lastPrice).toEqual('184.00')
+    expect(lastPrice).toEqual('184.00000000')
   })
 
 })
