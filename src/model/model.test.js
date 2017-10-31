d4ea29c0120f48b4878b63aaeadbc66fcfb06855
Israel Lima <israelhenrique@gmail.com>
2017-10-31 13:06:42 -0200

    add done to async tests

--- src/model/model.test.js
+++ src/model/model.test.js
@@ -5,50 +5,51 @@ import {
   usd_brl,
 } from './model'
 
-test('shows get latest traded price', () => {
+test('shows get latest traded price', (done) => {
   getPrice('ltc_brl','braziliex', (lastPrice) => {
-    expect(lastPrice).toEqual('184.00000000')
+    expect(lastPrice).toEqual('186.79999999')
+    done()
   })
-
 })
 
-test('get best ask price', () => {
+test('get best ask price', (done) => {
   getAskPrice('ltc_brl','braziliex', (askPrice) => {
     expect(askPrice).toEqual(186.9)
+    done()
   })
-
 })
 
-test('get best bid price', () => {
+test('get best bid price', (done) => {
   getBidPrice('ltc_brl','braziliex', (askPrice) => {
     expect(askPrice).toEqual(184.9)
+    done()
   })
-
 })
 
-test('shows get latest traded price at Wex', () => {
+test('shows get latest traded price at Wex', (done) => {
   getPrice('ltc_usd','wex', (lastPrice) => {
     expect(lastPrice).toEqual(56.835)
+    done()
   })
-
 })
 
-test('get best ask price at Wex', () => {
+test('get best ask price at Wex', (done) => {
   getAskPrice('ltc_usd','wex', (askPrice) => {
     expect(askPrice).toEqual(56.97)
+    done()
   })
-
 })
 
-test('get best bid price at Wex', () => {
+test('get best bid price at Wex', (done) => {
   getBidPrice('ltc_usd','wex', (askPrice) => {
     expect(askPrice).toEqual(56.7)
+    done()
   })
-
 })
 
-test('get usd x brl', () => {
+test('get usd x brl', (done) => {
   usd_brl((value) => {
     expect(value).toEqual(3.282902)
+    done()
   })
 })
