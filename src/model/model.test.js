1b749f4906e3b464e0bc9babdc3ddf751f0d707d
Israel Lima <israelhenrique@gmail.com>
2017-10-31 18:41:11 -0200

    mocking http requests with nock

--- src/model/model.test.js
+++ src/model/model.test.js
@@ -6,58 +6,133 @@ import {
   getBRLPrice,
 } from './model'
 
+var nock = require('nock');
+
+var wexnock = nock('https://wex.nz')
+                .get('/api/3/ticker/ltc_usd')
+                .reply(200, {
+                  'ltc_usd':{
+                    'high':57.49874,
+                    'low':56.1,
+                    'avg':56.79937,
+                    'vol':2898159.59804,
+                    'vol_cur':51161.35503,
+                    'last':56.14505,
+                    'buy':56.19,
+                    'sell':56.1001,
+                    'updated':1509476866}
+                 })
+                 .get('/api/3/depth/ltc_usd')
+                 .reply(200, {
+                   'ltc_usd':{
+                     'asks': [[56.4,0.1536],[56.478965,7.44058789]]
+                    }
+                 })
+                 .get('/api/3/depth/ltc_usd')
+                 .reply(200, {
+                   'ltc_usd':{
+                     'bids': [[56.140001,0.533677],[56.14,0.36877119]]
+                    }
+                 })
+                 .get('/api/3/ticker/ltc_usd')
+                 .reply(200, {
+                   'ltc_usd':{
+                     'high':57.49874,
+                     'low':56.1,
+                     'avg':56.79937,
+                     'vol':2898159.59804,
+                     'vol_cur':51161.35503,
+                     'last':56.14505,
+                     'buy':56.19,
+                     'sell':56.1001,
+                     'updated':1509476866}
+                  })
+
+var brexnock = nock('https://braziliex.com')
+                .get('/api/v1/public/ticker/ltc_brl')
+                .reply(200, {
+                  'active':1,
+                  'market':'ltc_brl',
+                  'last':'185.00000000',
+                  'percentChange':'-5.12',
+                  'baseVolume24':'86.056',
+                  'quoteVolume24':'15974.032',
+                  'baseVolume':'86.056',
+                  'quoteVolume':'15974.032',
+                  'highestBid24':'194.99000000',
+                  'lowestAsk24':'180.01000000',
+                  'highestBid':'182.05000000',
+                  'lowestAsk':'189.00000000'
+                 })
+                 .get('/api/v1/public/orderbook/ltc_brl')
+                 .reply(200, {
+                     'asks': [{'price':189,'amount':0.97583199},{'price':189.01,'amount':1.7616879}]
+                 })
+                 .get('/api/v1/public/orderbook/ltc_brl')
+                 .reply(200, {
+                     'bids': [{'price':183,'amount':9.86295081},{'price':182.01,'amount':5}]
+                 })
+
+var usdnock = nock('http://free.currencyconverterapi.com')
+              .persist()
+              .get('/api/v3/convert?q=USD_BRL&compact=y')
+              .reply(200, {
+                'USD_BRL':{'val':3.271397}
+               })
+
+
 test('convert last traded price to brl', (done) => {
   getBRLPrice('ltc_usd','wex', (lastPrice) => {
-    expect(lastPrice).toEqual(184.7678664)
+    expect(lastPrice).toEqual(183.67274813485)
     done()
   })
 })
 
 test('shows get latest traded price', (done) => {
   getPrice('ltc_brl','braziliex', (lastPrice) => {
-    expect(lastPrice).toEqual('186.79999999')
+    expect(lastPrice).toEqual('185.00000000')
     done()
   })
 })
 
 test('get best ask price', (done) => {
   getAskPrice('ltc_brl','braziliex', (askPrice) => {
-    expect(askPrice).toEqual(186.9)
+    expect(askPrice).toEqual(189)
     done()
   })
 })
 
 test('get best bid price', (done) => {
   getBidPrice('ltc_brl','braziliex', (askPrice) => {
-    expect(askPrice).toEqual(184.9)
+    expect(askPrice).toEqual(183)
     done()
   })
 })
 
 test('shows get latest traded price at Wex', (done) => {
   getPrice('ltc_usd','wex', (lastPrice) => {
-    expect(lastPrice).toEqual(56.835)
+    expect(lastPrice).toEqual(56.14505)
     done()
   })
 })
-
+//
 test('get best ask price at Wex', (done) => {
   getAskPrice('ltc_usd','wex', (askPrice) => {
-    expect(askPrice).toEqual(56.97)
+    expect(askPrice).toEqual(56.4)
     done()
   })
 })
 
 test('get best bid price at Wex', (done) => {
   getBidPrice('ltc_usd','wex', (askPrice) => {
-    expect(askPrice).toEqual(56.7)
+    expect(askPrice).toEqual(56.140001)
     done()
   })
 })
 
 test('get usd x brl', (done) => {
   usd_brl((value) => {
-    expect(value).toEqual(3.282902)
+    expect(value).toEqual(3.271397)
     done()
   })
 })
