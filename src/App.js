0a575bd1ed280a91e4a0ce0c0425ac2b3d3df177
Israel Lima <israelhenrique@gmail.com>
2017-11-08 18:54:32 -0200

    a simple interface

--- src/App.js
+++ src/App.js
@@ -1,21 +1,82 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {
+  getPrice,
+} from './model/model'
 
-class App extends Component {
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {exchange: ''}
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props._handleSubmit(this.state.exchange)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Exchange: </label>
+          <input name="exchange" type="text" value={this.state.exchange} onChange={this.handleChange} />
+          <br/>
+          <input type='submit' />
+        </form>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+class Panel extends React.Component {
+  render() {
+    return (
+      <div>{this.props.value}</div>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      value : '',
+    }
+  }
+
+  _handleSubmit(exchange) {
+    const self = this
+    getPrice('ltc_usd', exchange)
+      .then((value) => {
+        self.setState({
+          value: value,
+        })
+        console.log(value)
+      })
+  }
+
+  render () {
+    return (
+      <div>
+        <Form _handleSubmit={(exchange) => this._handleSubmit(exchange)}/>
+        <Panel value={this.state.value}/>
+      </div>
+
+    )
+  }
+}
+
+export default App
