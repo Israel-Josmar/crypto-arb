607008a41f1199a3522ba38af72957b8cb95f7b5
Israel Lima <israelhenrique@gmail.com>
2018-01-19 22:39:51 -0200

    order cards by percent profit

--- src/view/dashboard.js
+++ src/view/dashboard.js
@@ -45,6 +45,13 @@ class DashBoard extends React.Component {
     ]
   }
   render() {
+    const sortedCards = this.cards.sort(function (a, b) {
+      const nameA = a.profitPercent.toLowerCase()
+      const nameB = b.profitPercent.toLowerCase()
+      if (nameA > nameB) return -1
+      if (nameA < nameB) return 1
+      return 0 // default return value (no sorting)
+    })
     return (
       <div>
         <div className="container">
@@ -61,7 +68,7 @@ class DashBoard extends React.Component {
           </div>
           <div className="row">
             {
-              this.cards.map((card, index) => (
+              sortedCards.map((card, index) => (
                 <div key={index}  className="col">
                   <ExchangeCard exchange={card.exchange} criptocurrency={card.coin} profit={card.profit} profitPercent={card.profitPercent} />
                 </div>
