61909bff45cb755b75fbe1dd8622e1c0258ca15c
Israel Lima <israelhenrique@gmail.com>
2018-01-24 23:16:11 -0200

    first functional interface

--- src/view/dashboard.js
+++ src/view/dashboard.js
@@ -27,6 +27,11 @@ function Button(props) {
 class DashBoard extends React.Component {
   constructor() {
     super()
+    this.state = {
+      value: '',
+      cost: '',
+      cards: [],
+    }
     this.cards =  [
       {
         exchange: 'Exchange2', coin: 'Litecoin', profit: '400.00', profitPercent: '4%',
@@ -44,9 +49,32 @@ class DashBoard extends React.Component {
         exchange: 'Exchange5', coin: 'Litecoin', profit: '100.00', profitPercent: '1%',
       },
     ]
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+  handleChange(event) {
+    const target = event.target
+    const name =  target.name
+
+    this.setState({
+      [name]: target.value,
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const data = this.fetchCardsData(this.state.value, this.state.cost)
+    this.setState({ cards: data })
+  }
+
+  fetchCardsData(value, cost) {
+    return this.cards
+    // return fetchAPI(value, cost)
+  }
+
   render() {
-    const sortedCards = this.cards.sort(function (a, b) {
+    const sortedCards = this.state.cards.sort(function (a, b) {
       const nameA = a.profitPercent.toLowerCase()
       const nameB = b.profitPercent.toLowerCase()
       if (nameA > nameB) return -1
@@ -58,12 +86,14 @@ class DashBoard extends React.Component {
         <div className="container">
           <div className="row pb-2">
             <div className="col">
-              <form>
+              <form onSubmit={this.handleSubmit}>
                 <div className="form-row">
                   <div className="col-auto">
                     <div className="form-group">
                       <label htmlFor="value">Investment Value</label>
-                      <input type="number" className="form-control" id="value" placeholder="1000" />
+                      <input type="number" value={this.state.value} onChange={this.handleChange} className="form-control" id="value" name="value" placeholder="1000" />
+                      <label htmlFor="cost">Deposit Cost</label>
+                      <input type="number" value={this.state.cost} onChange={this.handleChange} className="form-control" id="cost" name="cost" placeholder="300" />
                     </div>
                   </div>
                 </div>
