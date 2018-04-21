60a42e1250f26b1f093ea7eeeff2dceea47ef73e
Israel Lima <israelhenrique@gmail.com>
2018-04-21 19:00:40 -0300

    set up enzyme

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,7 @@
 // use node-fetch on test enviroment (instead of isomorphic-fetch)
 // it works better with nock library
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import fetch from 'node-fetch'
+Enzyme.configure({ adapter: new Adapter() })
 global.fetch = fetch
