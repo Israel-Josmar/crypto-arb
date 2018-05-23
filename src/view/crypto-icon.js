a962c049c34205aa86ec501ebc019ddac0df7d49
Israel Lima <israelhenrique@gmail.com>
2018-05-23 19:57:54 -0300

    use getCryptoLogo function to get icon src

--- src/view/crypto-icon.js
+++ src/view/crypto-icon.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-import * as Cryptos from './cryptos'
+import { getCryptoLogo }  from './cryptos'
 
 export const CryptoIcon = ({
   id,
 }) => {
   return (
-    // eslint-disable-next-line
-    <img src={Cryptos.logos[id]} alt=""/>
+    <img src={getCryptoLogo(id)} alt=""/>
   )
 }
