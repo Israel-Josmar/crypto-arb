d0e6ef53437674e2b028a7bc133f2c8446e8c762
Israel Lima <israelhenrique@gmail.com>
2018-07-19 20:43:52 -0300

    remove inline css

--- src/components/exchange-card.js
+++ src/components/exchange-card.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Cryptocurrency } from './crypto-currency'
 import { NumberDisplay } from './number-display'
 
+import './exchange-card.css'
+
 export const ExchangeCard = ({
   exchangeFrom,
   exchangeTo,
@@ -9,17 +11,13 @@ export const ExchangeCard = ({
   profitPercent,
   criptocurrency,
 }) => {
-  const style = {
-    width: '240px',
-    height: '208px',
-  }
   return (
-    <div className="card" style={style}>
+    <div className="card" >
       <div className="card-header d-flex flex-column">
         <span className="align-self-center"><Cryptocurrency id={criptocurrency}/></span>
       </div>
       <div className="card-body d-flex align-items-center" >
-        <div className="d-flex mr-auto"><span style={{ color: '#28a745' }}>R$&nbsp;</span><NumberDisplay value={profit} /></div>
+        <div className="d-flex mr-auto"><span className="profit">R$&nbsp;</span><NumberDisplay value={profit} /></div>
         <div className="ml-auto"><NumberDisplay value={profitPercent} showAspercent="true"/></div>
       </div>
       <div className="card-footer text-muted d-flex">
