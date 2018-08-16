b81e1c1ec977c52c4b6a491a7c2943b2482c1a1b
Israel Lima <israelhenrique@protonmail.com>
2018-08-16 20:50:21 -0300

    lift up user data on login

--- src/pages/login-page.js
+++ src/pages/login-page.js
@@ -5,22 +5,15 @@ import { login } from '../services/login-service'
 import './login-page.css'
 
 export class LoginPage extends Component {
-  state = {
-    user_name: '',
-  }
-
   handleSubmit = async (username, password) => {
     const response = await login(username, password)
-    this.setState({
-      user_name: response.user_name,
-    })
+    this.props.onLogin(response.user_name)
   }
 
   render() {
     return (
       <div className="login-form">
         <LoginForm onSubmit={this.handleSubmit}/>
-        {this.state.user_name}
       </div>
     )
   }
--- src/pages/login-page.test.js
+++ src/pages/login-page.test.js
@@ -7,8 +7,10 @@ import { LoginForm } from '../components/login-form'
 describe('<LoginPage /> component', () => {
   let root
 
+  const mockCallBack = jest.fn()
+
   beforeAll(() => {
-    root = shallow(<LoginPage />)
+    root = shallow(<LoginPage onLogin={mockCallBack}/>)
   })
 
   test('should be defined', () => {
@@ -17,5 +19,11 @@ describe('<LoginPage /> component', () => {
 
   test('should have LoginForm component', () => {
     expect(root.find(LoginForm)).toHaveLength(1)
+  })
+
+  test('should call onLogin prop on LoginForm submit', async () => {
+    const form = root.find(LoginForm)
+    await form.prop('onSubmit')()
+    expect(mockCallBack).toHaveBeenCalled()
   })
 })
